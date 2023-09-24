550db369286cd810c51f044e3d72fab2898432f9
Lucas_yoboy <lucasheide62@gmail.com>
2023-09-24 11:15:31 -0400

    update

--- src/lib/mysql.js
+++ src/lib/mysql.js
@@ -1,5 +1,5 @@
 import mysql from "mysql2/promise";
-
+import { PUBLIC_DATABASE_HOST, PUBLIC_DATABASE_PASSWORD, PUBLIC_DATABASE, PUBLIC_DATABASE_USER } from '$env/static/public';
 let mysqlconn = null;
 
 export function mysqlconnFn() {
@@ -12,10 +12,10 @@ export function mysqlconnFn() {
     //   database: "statedata",
     // });
     mysqlconn = mysql.createConnection({
-      host: "134.65.234.251",
-      user: "root",
-      password: "@Alandro34",
-      database: "villerosmc",
+      host: PUBLIC_DATABASE_HOST,
+      user: PUBLIC_DATABASE_USER,
+      password: PUBLIC_DATABASE_PASSWORD,
+      database: PUBLIC_DATABASE,
     });
   }
 
