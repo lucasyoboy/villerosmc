7edfd607a1ab681dd941d483a9b2b73850dd5ab3
Lucas_yoboy <lucasheide62@gmail.com>
2024-02-14 23:14:12 -0300

    update

--- src/routes/profile/[username]/+page.server.js
+++ src/routes/profile/[username]/+page.server.js
@@ -14,7 +14,7 @@ export async function load({params, url}) {
 		}
 	})
 	
-    const response = await fetch('http://localhost:5173/api/profile?username='+params.username+'');
+    const response = await fetch('http://localhost/api/profile?username='+params.username+'');
     let stats = await response.json();
 
 	return {
